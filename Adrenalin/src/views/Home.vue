4cb357321a8ed3ab662fa1905e5bcfae00676299
Kristina Gordey <kristinababichvic@gmail.com>
2025-06-10 12:18:50 +0300

    setting up the environment

---
+++ Adrenalin/src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import "../assets/main.css";
+import HeaderTop from "@/components/HeaderTop.vue";
+</script>
+<template>
+  <div class="wrapper">
+    <HeaderTop></HeaderTop>
+  </div>
+</template>
+<style scoped></style>
